caa9cc93acdb0b3871206f85ff07004e2c721228
Ramazan <coolstoves@gmail.com>
2025-06-14 09:42:32 +0300

    1406250942

--- Tabloyu_Degistirme.sql
+++ Tabloyu_Degistirme.sql
@@ -41,4 +41,72 @@ SELECT * FROM kitaplar;
 ALTER TABLE kitaplar
 ADD sayfa_sayisi INT; 
 
-SELECT * FROM kitaplar;
+
+
+/*
+ALTER TABLE-Sütunun Veri Türünü Değiştirme
+*/
+
+/*
+
+*****************SYNTAX****************
+ALTER TABLE tablo_adi
+MODIFY COLUMN sütun_adi + yeniDataType
+
+*/
+
+/*
+ogrenciler tablosundakşi kayıt yılı column'inin data type ini degisitiriniz
+*/
+
+ALTER TABLE ogrenciler
+MODIFY COLUMN ortalama_not DOUBLE;
+
+ALTER TABLE ogrenciler
+MODIFY COLUMN kayit_yili year;
+
+/*
+
+ALTER TABLE -SUTUN Adını Değiştirme
+
+*/
+
+/*
+
+*****************SYNTAX****************
+ALTER TABLE tablo_adi
+RENAME COLUMN eskiSütun_adi TO yeniSütunAdi
+
+*/
+
+/*
+personel tablosundakşi pozisyon column'inin  ad ini sorumluluk olarak degisitiriniz
+*/
+
+ALTER TABLE personel
+RENAME COLUMN pozisyon TO sorumluluk;
+
+/*
+
+ALTER TABLE -SUTUN silme
+
+*/
+
+/*
+
+*****************SYNTAX****************
+ALTER TABLE tablo_adi
+DROP COLUMN Sütun_adi 
+
+*/
+
+/*
+ogrenciler tablosundaki ogrenci id column'ini siliniz.
+*/
+ALTER TABLE ogrenciler
+DROP COLUMN ogrenci_id;
+
+/*
+silme işleminden önce etraflıca düşünülmeli geri dönüşümü 
+zordur ileride ihtiyac duyulması muhtemel bilgiler olabiliri
+*/
